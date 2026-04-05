5d85c8e379acd40ce72a6068d38c5c9df8bacdf9
Arno Deceuninck <arnodece@gmail.com>
2026-04-05 18:34:29 +0200

    fix timezone warning (and deprecated feature)

--- backend-framework/db/mixins.py
+++ backend-framework/db/mixins.py
@@ -3,9 +3,9 @@ from sqlalchemy import Column, DateTime, String
 from sqlalchemy.orm import DeclarativeBase
 
 class AuditMixin:
-    modified_date = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    modified_date = Column(DateTime, default=datetime.now(datetime.timezone.utc), onupdate=datetime.utcnow)
     modified_by = Column(String, nullable=True)
-    created_date = Column(DateTime, default=datetime.utcnow)
+    created_date = Column(DateTime, default=datetime.now(datetime.timezone.utc))
 
 class Base(AuditMixin, DeclarativeBase):
     pass
